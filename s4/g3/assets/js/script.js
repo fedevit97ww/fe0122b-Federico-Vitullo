1e3dd70eac095cb8e60b9ccaa03ae064fdc2a9e2
fedevit97ww <bnfederico.vitullo210@gmail.com>
2022-02-16 18:38:27 +0100

    g3s4

---
+++ s4/g3/assets/js/script.js
@@ -0,0 +1,55 @@
+function sottrazione(corrente, passato ,destinazione){
+var data = document.getElementById(corrente).value *1;
+ data -= document.getElementById(passato).innerHTML *1;
+ document.getElementById(destinazione).innerHTML = (data) ;
+
+}
+
+/*function operazioni() {
+    var num = 9;
+    function moltiplicazione()
+     {
+        console.log(num * num);
+    }
+  
+  moltiplicazione();
+   
+}
+operazioni();
+*/
+operazioni = () =>{
+    var num=9;
+    moltiplicazione=()=> console.log(num*num);
+moltiplicazione()
+}
+operazioni();
+
+
+
+/*function saluta(){
+    console.log('ciao')
+}*/
+
+(saluta = () => console.log('Ciao a tutti')) ();
+
+
+function controlloEta(){
+   eta = document.getElementById('tuaeta').value *1;
+riferimento = 18;
+massimo = 121;
+if (eta < riferimento)  {alert('non puoi');}
+else if (eta > massimo)  {alert('stai crepando');}
+else {
+    alert('puoi accedere'); //scoprire come creare un link ad una pagina 
+}
+
+}
+function verifica(){
+    var anni =document.getElementById('tuaeta').value *1;
+    var anniGiusti = 22;
+    if (anni != anniGiusti) {console.log("non hai l'età giusta");}
+ else if (anni == anniGiusti) {console.log("hai l'età giusta");}
+
+} //da chiedere come far funzionare questa function insieme alla console
+verifica()
+
