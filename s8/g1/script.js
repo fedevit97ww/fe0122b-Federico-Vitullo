d1d90a870478edd6ea6ba60b584b453d4fc32c1a
fedevit97ww <bnfederico.vitullo210@gmail.com>
2022-03-14 17:54:36 +0100

    esg1s8

--- s8/g1/script.js
+++ s8/g1/script.js
@@ -1,7 +1,7 @@
 var giocatore1 = Math.floor((Math.random() * (100 - 1) + 1));
 var giocatore2 = Math.floor((Math.random() * (100 - 1) + 1));
-console.log(giocatore1);
-console.log(giocatore2);
+console.log('giocatore1' + " " + giocatore1);
+console.log('giocatore2' + " " + giocatore2);
 var numero = Math.floor((Math.random() * (100 - 1) + 1));
 console.log(numero);
 var diff1 = Math.abs(numero - giocatore1);
@@ -21,7 +21,3 @@ else if (giocatore1 == numero) {
 else if (giocatore2 == numero) {
     console.log('gioc2 ha vinto');
 }
-//giocatore1 dice 5
-//giocatore dice 12
-//numero = 10
-// 12 -10 = 2 //  5-10 = -5 if ris gioc2 -numero > ris gioc1 -numero allora gioc2 è andato più vicino
