0ddcb8d357a6fae273677292d49eb980c7572317
fedevit97ww <bnfederico.vitullo210@gmail.com>
2022-03-14 17:28:15 +0100

    es g1s8

---
+++ s8/g1/script.ts
@@ -0,0 +1,32 @@
+let giocatore1: number = Math.floor((Math.random() * (100 - 1) + 1));
+let giocatore2: number = Math.floor((Math.random() * (100 - 1) + 1));
+
+console.log(giocatore1)
+console.log(giocatore2)
+
+let numero: number = Math.floor((Math.random() * (100 - 1) + 1))
+console.log(numero)
+
+let diff1 = Math.abs(numero - giocatore1)
+let diff2 = Math.abs(numero - giocatore2 )
+
+if (diff1 > diff2 && diff2 !=0) {
+    console.log('gioc2 ci è andato più vicino')
+
+    } else if (diff1 < diff2 && diff1 !=0) {
+        console.log('gioc1 ci è andato più vicino')
+
+    } else if (giocatore1 == giocatore2) {
+        console.log('hanno pareggiato')
+
+    } else if (giocatore1 == numero) {
+        console.log('gioc1 ha vinto')
+        
+    } else if (giocatore2 == numero) {
+        console.log('gioc2 ha vinto')
+    }
+//giocatore1 dice 5
+//giocatore dice 12
+//numero = 10
+// 12 -10 = 2 //  5-10 = -5 if ris gioc2 -numero > ris gioc1 -numero allora gioc2 è andato più vicino
+
