e31b2b73e123255151223f1e8675c57030ad4c9d
fedevit97ww <bnfederico.vitullo210@gmail.com>
2022-03-18 01:30:36 +0100

    ProgettoSett8

--- s8/progetto/script.js
+++ s8/progetto/script.js
@@ -1,23 +1,25 @@
 fetch('Abbigliamento.json')
     .then(function (response) { return response.json(); })
     .then(function (risposta) {
+    var contVestiti = [];
     risposta.forEach(function (item) {
         var vestiti = new Abbigliamento(item.id, item.codprod, item.collezione, item.capo, item.modello, item.quantita, item.colore, item.prezzoivaesclusa, item.prezzoivainclusa, item.disponibile, item.saldo);
+        contVestiti.push(vestiti);
         console.log(vestiti);
         console.log('xxxxxxxxxxxxx');
         console.log('xxxxxxxxxxxxx');
         console.log('Il prezzo IVA esclusa del capo ' + item.capo + ' è di ' + item.prezzoivaesclusa + ' applicando uno sconto del ' + item.saldo + '%' + ' il totale imponibile risulta: ' + vestiti.getSaldocapo());
         console.log('Il prezzo del capo ' + item.capo + ' scontato del ' + item.saldo + '% è: ' + vestiti.getAcquistoCapo() + ' IVA inclusa');
     });
-    console.table(vestito);
-    console.table(vestito2);
-    console.table(vestito3);
-    console.log(vestito.getSaldocapo());
-    console.log(vestito2.getSaldocapo());
-    console.log(vestito3.getSaldocapo());
-    console.log(vestito.getAcquistoCapo());
-    console.log(vestito2.getAcquistoCapo());
-    console.log(vestito3.getAcquistoCapo());
+    for (var i = 0; i < contVestiti.length; i++) {
+        console.log(contVestiti[i].getSaldocapo());
+    }
+    //elementi creati a mano
+    contVestiti.push(vestito);
+    contVestiti.push(vestito2);
+    contVestiti.push(vestito3);
+    //
+    populateSelectOptions(contVestiti);
 });
 //CLASSE//
 var Abbigliamento = /** @class */ (function () {
@@ -45,3 +47,43 @@ var Abbigliamento = /** @class */ (function () {
 var vestito = new Abbigliamento(6, 1727, 'estate', 'canotta', 1245, 6, 'bianco', 100, 122, 'negozio', 10);
 var vestito2 = new Abbigliamento(7, 8712, 'autunno', 'cappello', 8945, 7, 'verde', 10, 12, 'magazzino', 8);
 var vestito3 = new Abbigliamento(8, 9832, 'inverno', 'sciarpa', 1597, 6, 'giallo', 20, 24.4, 'negozio', 10);
+//var globali
+var select = document.querySelector('#vestiti');
+///funzioni
+function populateSelectOptions(p) {
+    for (var i = 0; i < p.length; i++) {
+        var option = document.createElement('option');
+        option.setAttribute('value', p[i].id);
+        option.innerText = "".concat(p[i].capo.toUpperCase(), " ");
+        select.append(option);
+    }
+    select.addEventListener('change', function mostraVestiti() {
+        var idVestito = select.value;
+        //let autoSelezionata = veicoli.find(auto => auto.id == idAutoSel)
+        var vestitoSel;
+        for (var i = 0; i < p.length; i++) {
+            if (p[i].id == idVestito) {
+                vestitoSel = p[i];
+            }
+        }
+        var collect = document.querySelector('#collezione');
+        collect.innerText = vestitoSel.collezione.toUpperCase();
+        var capo = document.querySelector('#capo');
+        capo.innerText = vestitoSel.capo.toUpperCase();
+        var quantita = document.querySelector('#quantita');
+        quantita.innerText = vestitoSel.quantita;
+        var colore = document.querySelector('#colore');
+        colore.innerText = vestitoSel.colore.toUpperCase();
+        var ivaInclusa = document.querySelector('#ivaInclusa');
+        ivaInclusa.innerText = vestitoSel.prezzoivainclusa + '€';
+        var disponibile = document.querySelector('#disponibile');
+        disponibile.innerText = vestitoSel.disponibile.toUpperCase();
+        var saldo = document.querySelector('#saldo');
+        saldo.innerText = vestitoSel.saldo + '%';
+        var scontato = document.querySelector('#scontato');
+        var sconto = document.querySelector('.btn-primary');
+        sconto.addEventListener('click', function () {
+            scontato.innerText = vestitoSel.getAcquistoCapo() + '€';
+        });
+    });
+}
--- s8/progetto/script.ts
+++ s8/progetto/script.ts
@@ -1,9 +1,12 @@
 fetch('Abbigliamento.json')
     .then(response => response.json())
     .then(risposta => {
+        let contVestiti: any = [];
         risposta.forEach((item: Abbigliamento) => {
 
             var vestiti = new Abbigliamento(item.id, item.codprod, item.collezione, item.capo, item.modello, item.quantita, item.colore, item.prezzoivaesclusa, item.prezzoivainclusa, item.disponibile, item.saldo);
+
+            contVestiti.push(vestiti)
 
             console.log(vestiti);
             console.log('xxxxxxxxxxxxx')
@@ -11,25 +14,24 @@ fetch('Abbigliamento.json')
             console.log('Il prezzo IVA esclusa del capo ' + item.capo + ' è di ' + item.prezzoivaesclusa + ' applicando uno sconto del ' + item.saldo + '%' + ' il totale imponibile risulta: ' + vestiti.getSaldocapo());
 
             console.log('Il prezzo del capo ' + item.capo + ' scontato del ' + item.saldo + '% è: ' + vestiti.getAcquistoCapo() + ' IVA inclusa');
+        });
 
-            
-        
-        
-        })
-      
-        console.table(vestito);
-        console.table(vestito2);
-        console.table(vestito3);
 
-        console.log(vestito.getSaldocapo());
-        console.log(vestito2.getSaldocapo());
-        console.log(vestito3.getSaldocapo());
+        for (let i = 0; i < contVestiti.length; i++) {
+            console.log(contVestiti[i].getSaldocapo());
+        }
 
-        console.log(vestito.getAcquistoCapo());
-        console.log(vestito2.getAcquistoCapo());
-        console.log(vestito3.getAcquistoCapo());
+        //elementi creati a mano
+        contVestiti.push(vestito)
+        contVestiti.push(vestito2)
+        contVestiti.push(vestito3)
+        //
+        populateSelectOptions(contVestiti)
+
+
     })
-    
+
+
 //CLASSE//
 class Abbigliamento {
     id: number;
@@ -69,3 +71,51 @@ let vestito = new Abbigliamento(6, 1727, 'estate', 'canotta', 1245, 6, 'bianco',
 let vestito2 = new Abbigliamento(7, 8712, 'autunno', 'cappello', 8945, 7, 'verde', 10, 12, 'magazzino', 8)
 let vestito3 = new Abbigliamento(8, 9832, 'inverno', 'sciarpa', 1597, 6, 'giallo', 20, 24.4, 'negozio', 10)
 
+
+
+//var globali
+var select: any = document.querySelector('#vestiti')
+
+///funzioni
+function populateSelectOptions(p: any) {
+
+    for (var i = 0; i < p.length; i++) {
+        var option = document.createElement('option')
+        option.setAttribute('value', p[i].id)
+        option.innerText = `${p[i].capo.toUpperCase()} `
+        select.append(option)
+    }
+
+    select.addEventListener('change', function mostraVestiti() {
+        var idVestito = select.value;
+        //let autoSelezionata = veicoli.find(auto => auto.id == idAutoSel)
+        var vestitoSel: any;
+        for (var i = 0; i < p.length; i++) {
+            if (p[i].id == idVestito) {
+                vestitoSel = p[i]
+            }
+        }
+        let collect: any = document.querySelector('#collezione');
+        collect.innerText = vestitoSel.collezione.toUpperCase();
+        let capo: any = document.querySelector('#capo');
+        capo.innerText = vestitoSel.capo.toUpperCase();
+        let quantita: any = document.querySelector('#quantita');
+        quantita.innerText = vestitoSel.quantita;
+        let colore: any = document.querySelector('#colore');
+        colore.innerText = vestitoSel.colore.toUpperCase();
+        let ivaInclusa: any = document.querySelector('#ivaInclusa');
+        ivaInclusa.innerText = vestitoSel.prezzoivainclusa + '€';
+        let disponibile: any = document.querySelector('#disponibile');
+        disponibile.innerText = vestitoSel.disponibile.toUpperCase();
+        let saldo: any = document.querySelector('#saldo');
+        saldo.innerText = vestitoSel.saldo + '%';
+
+        let scontato: any = document.querySelector('#scontato')
+        let sconto: any = document.querySelector('.btn-primary');
+        sconto.addEventListener('click', () => {
+            scontato.innerText = vestitoSel.getAcquistoCapo() + '€'
+        });
+    });
+
+
+}
